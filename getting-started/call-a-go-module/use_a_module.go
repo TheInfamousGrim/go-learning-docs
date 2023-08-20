0cbbe60f32e3e08070e5863b705542f1aecf03c2
George Fincher <george@grimfunky.dev>
2023-08-21 00:12:58 +0100

    Feat ✨: Add a test for the Hello function

--- getting-started/call-a-go-module/use_a_module.go
+++ getting-started/call-a-go-module/use_a_module.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message print it.
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	// Set properties of the predefined Logger, including
+	// The log entry prefix and a flag to disable printing
+	// The time, source file, and line number.
+	log.SetPrefix("Greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request for greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(messages)
 }
